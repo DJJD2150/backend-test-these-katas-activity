9f7e76aafaacffa0db0ae85054dc3a0640aa9f7b
Jackson Nathaniel Detke <jndetke2150@gmail.com>
2020-06-23 14:32:20 -0400

    Initial commit to DJJD2150's Github repo.

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -4,19 +4,34 @@ import katas
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(katas.add(3, 5), 8)
+        self.assertEqual(katas.add(-1, 1), 0)
+        self.assertEqual(katas.add(-4, -7), -11)
+        self.assertEqual(katas.add(14, 21), 35)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(katas.multiply(3, 5), 15)
+        self.assertEqual(katas.multiply(-1, 1), -1)
+        self.assertEqual(katas.multiply(-4, -7), 28)
+        self.assertEqual(katas.multiply(14, 21), 294)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(katas.power(3, 5), 243)
+        self.assertEqual(katas.power(10, 6), 1000000)
+        self.assertEqual(katas.power(8, 2), 64)
+        self.assertEqual(katas.power(2, 12), 4096)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        self.assertEqual(katas.factorial(5), 120)
+        self.assertEqual(katas.factorial(7), 5040)
+        self.assertEqual(katas.factorial(3), 6)
+        self.assertEqual(katas.factorial(2), 2)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        self.assertEqual(katas.fibonacci(5), 3)
+        self.assertEqual(katas.fibonacci(8), 13)
+        self.assertEqual(katas.fibonacci(10), 34)
+        self.assertEqual(katas.fibonacci(), 1)
 
 
 if __name__ == '__main__':
